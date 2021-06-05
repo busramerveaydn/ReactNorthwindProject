2baa4484659a8a0184645898f36b575b82f1393a
Büşra Merve AYDIN <busramerveaydn@gmail.com>
2021-06-06 00:21:16 +0300

    added

---
+++ src/layouts/Dashboard.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ProductList from '../pages/ProductList'
+import Categories from './Categories'
+import { Grid, GridRow} from 'semantic-ui-react'
+
+export default function Dashboard() {
+    return (
+        <div>
+            <Grid>
+                <GridRow>
+                    <Grid.Column width={4}>
+                        <Categories/>
+                    </Grid.Column>
+                    <Grid.Column width={12}>
+                        <ProductList/>
+                    </Grid.Column>
+                </GridRow>
+            </Grid>
+        </div>
+    )
+}
