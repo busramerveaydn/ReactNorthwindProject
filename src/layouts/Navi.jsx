e009369db1c2e636328b0f40d3b4c77d2c36fa76
Büşra Merve AYDIN <busramerveaydn@gmail.com>
2021-06-09 23:46:29 +0300

    9 Haziran 23:46

--- src/layouts/Navi.jsx
+++ src/layouts/Navi.jsx
@@ -1,15 +1,17 @@
-import React, {useState}from 'react'
+import React, { useState } from 'react'
 import CartSummary from './CartSummary'
 import { Container, Menu } from 'semantic-ui-react'
 import SignedOut from './SignedOut'
 import SignedIn from './SignedIn'
+import { useHistory } from 'react-router'
 
 export default function Navi() {
 
     const [isAuthenticated, setIsAuthenticated] = useState(true)
-
+    const history = useHistory()
     function handleSignOut(params) {
         setIsAuthenticated(false)
+        history.push("/")
     }
 
     function handleSignIn(params) {
@@ -20,12 +22,12 @@ export default function Navi() {
         <div>
             <Menu inverted fixed="top">
                 <Container>
-                    <Menu.Item name='home'/>
-                    <Menu.Item name='messages'/>
+                    <Menu.Item name='home' />
+                    <Menu.Item name='messages' />
 
                     <Menu.Menu position='right'>
-                        <CartSummary/>
-                        {isAuthenticated?<SignedIn signOut={handleSignOut}/>:<SignedOut singIn={handleSignIn}/>}
+                        <CartSummary />
+                        {isAuthenticated ? <SignedIn signOut={handleSignOut} /> : <SignedOut singIn={handleSignIn} />}
                     </Menu.Menu>
                 </Container>
             </Menu>
