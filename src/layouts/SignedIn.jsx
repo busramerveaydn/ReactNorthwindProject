f917a443282e259f02851f2e6a99cd4c8b4b7aaa
Büşra Merve AYDIN <busramerveaydn@gmail.com>
2021-06-09 22:45:07 +0300

    09-06-2021

--- src/layouts/SignedIn.jsx
+++ src/layouts/SignedIn.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Dropdown, Menu,Image } from 'semantic-ui-react'
 
-export default function SignedIn() {
+export default function SignedIn(props) {
     return (
         <div>
             <Menu.Item>
                 <Image avatar spaced="right"  />
-                <Dropdown pointing="top left" text="Büşra Merve">
+                <Dropdown pointing="top left"  text="Büşra Merve">
                     <Dropdown.Menu>
                         <Dropdown.Item text="Bilgilerim" icon="info"/>
-                        <Dropdown.Item text="Çıkış Yap" icon="sign-out"/>
+                        <Dropdown.Item onClick={props.signOut} text="Çıkış Yap" icon="sign-out"/>
                     </Dropdown.Menu>
                 </Dropdown>
             </Menu.Item>
--- src/layouts/SignedOut.jsx
+++ src/layouts/SignedOut.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Button, Menu } from 'semantic-ui-react'
 
-export default function SignedOut() {
+export default function SignedOut(props) {
     return (
         <div>
             <Menu.Item>
-                <Button primary >Giriş Yap</Button>
-                <Button primary style={{marginright:"0.5em"}}>Kayıt Ol</Button>
+                <Button primary onClick={props.singIn} >Giriş Yap</Button>
+                <Button primary style={{marginleft:"0.5em"}}>Kayıt Ol</Button>
             </Menu.Item>
         </div>
     )
